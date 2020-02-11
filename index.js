d007e1077625d830952cf604f9dfe9c296556a12
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-02-11 00:37:31 -0800

    added search bar, working on functionality

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
 'use strict';
 
+//TO-DO: FOLLOW TUTORIAL TO GET API POC TO WORK WITH GOOGLE LIBRARY, THEN SEE WHERE MORE JQUERY CAN BE APPLIED.
+//TO-DO: FOLLOW TUTORIAL FOR API POC AND THEN BREAK UP SO IT LOOKS LESS LIKE SPAGHETTI CODE. PRIORITIZE WHAT TO INITIALIZE.
+//TO-DO: HOW TO INTEGRATE MORE FETCH? => CORS-ANYWHERE? 
+//TO-DO: FIND WAY TO INTEGRATE SOME JSON MANIPULATION AFTER MAP SEARCH WORKS. 
+
 const testKey = "AIzaSyBBydFcLANTMh9X7_9S-0YpZuy7bPNFAYY";
 const foodBankSearch = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
 
-var map; 
-function initMap() {
+function initAutocomplete() {
     console.log("ran!");
+
     let map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 32.714286, lng: -117.155577},
         scrollwheel: false,
-        zoom: 12
+        zoom: 12,
+        maptypeId: 'roadmap'
     });
-}   
 
-$(initMap);
+    let input = document.getElementById('pac-input'); // can maybe do $().val() here??
+    let searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+  
+    let markers = [];
+
+    searchBox.addListener('places_changed', function() {
+      let places = searchBox.getPlaces();
+  
+      if (places.length == 0) {
+        return;
+      }
+
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      let bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        let icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+  
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+  
+        if (place.geometry.viewport) {
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
+
+
+// OLD CODE BELOW, FETCH DID NOT WORK BECAUSE OF HOW THE API WORKS. NEEDED TO USE SPECIFIC MAPS JS API LIBRARY.
 
 // function submitFoodBank() {
 //     console.log("app loaded!");
