67d2a073571167f069091b813d5cdabf014897b1
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-02-10 16:38:17 -0800

    starting to implement map and search functions

--- index.js
+++ index.js
@@ -3,50 +3,61 @@
 const testKey = "AIzaSyBBydFcLANTMh9X7_9S-0YpZuy7bPNFAYY";
 const foodBankSearch = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
 
-function submitFoodBank() {
-    console.log("app loaded!");
-    $("#food-bank-selector").on("submit", function(event) {
-        event.preventDefault();
-        const bankEntry = $(this).find("#food-bank").val();
-        getFoodBank(bankEntry);
+var map; 
+function initMap() {
+    console.log("ran!");
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 32.714286, lng: -117.155577},
+        scrollwheel: false,
+        zoom: 12
     });
-}
+}   
 
-function convertQuery(parameters) {
-    const searchData = Object.keys(parameters).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(parameters[key])}`);
-    return searchData.join("&");
-}
+$(initMap);
 
-function getFoodBank(bankEntry) {
-    const parameters = { 
-        input: bankEntry,
-        inputtype: 'textquery',
-        fields: ['formatted_address', 'geometry', 'icon,name', 'permanently_closed', 'photos', 'place_id', 'plus_code', 'types'],
-        key: testKey,
-    };
+// function submitFoodBank() {
+//     console.log("app loaded!");
+//     $("#food-bank-selector").on("submit", function(event) {
+//         event.preventDefault();
+//         const bankEntry = $(this).find("#food-bank").val();
+//         getFoodBank(bankEntry);
+//     });
+// }
 
-    const searchString = convertQuery(parameters);
-    const searchQuery = foodBankSearch + '?' + searchString;
-    console.log(searchQuery);
+// function convertQuery(parameters) {
+//     const searchData = Object.keys(parameters).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(parameters[key])}`);
+//     return searchData.join("&");
+// }
 
-    fetch(searchQuery, {
-        mode: 'no-cors',
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-            })
-        .then(responseJson => showFoodBank(responseJson))
-        .catch(error => alert("Something went wrong. Try again later."));
-}
+// function getFoodBank(bankEntry) {
+//     const parameters = { 
+//         input: bankEntry,
+//         inputtype: 'textquery',
+//         fields: ['formatted_address', 'geometry', 'icon,name', 'permanently_closed', 'photos', 'place_id', 'plus_code', 'types'],
+//         key: testKey,
+//     };
 
-function showFoodBank(responseJson) {
-    $("#food-bank-list").empty();
-    $("#food-bank-list").append(`
-    <li><h3>${responseJson.data.name}</h3>`);
-    $("#search-results").removeClass("hidden");
-}
+//     const searchString = convertQuery(parameters);
+//     const searchQuery = foodBankSearch + '?' + searchString;
+//     console.log(searchQuery);
 
-$(submitFoodBank);
+//     fetch(searchQuery)
+//         .then(response => {
+//             if (response.ok) {
+//                 return response.json();
+//             }
+//             throw new Error(response.statusText);
+//             })
+//         .then(responseJson => showFoodBank(responseJson))
+//         .catch(error => alert("Something went wrong. Try again later."));
+// }
+
+// function showFoodBank(responseJson) {
+//     console.log(responseJson);
+//     $("#food-bank-list").empty();
+//     $("#food-bank-list").append(`
+//     <li><h3>${responseJson.data.name}</h3>`);
+//     $("#search-results").removeClass("hidden");
+// }
+
+// $(submitFoodBank);
