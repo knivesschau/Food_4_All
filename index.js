96fb379e39eadcd97194b9d027f48fd0f0b13de1
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-02-14 19:20:25 -0800

    added markers

--- index.js
+++ index.js
@@ -6,9 +6,6 @@
 //TO-DO: FIND WAY TO INTEGRATE SOME JSON MANIPULATION AFTER MAP SEARCH WORKS. 
 //TO-DO: STYLE!
 
-const testKey = "AIzaSyBBydFcLANTMh9X7_9S-0YpZuy7bPNFAYY";
-const foodBankSearch = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
-
 function navigationListener() {
   $("#food-bank").on('click', function(event) {
       $(".food-bank-page").show().removeClass("hidden"); 
@@ -55,8 +52,8 @@ function initAutocomplete() {
           console.log("Returned place contains no geometry");
           return;
         }
+
         let icon = {
-          url: place.icon,
           size: new google.maps.Size(71, 71),
           origin: new google.maps.Point(0, 0),
           anchor: new google.maps.Point(17, 34),
@@ -65,6 +62,7 @@ function initAutocomplete() {
   
         markers.push(new google.maps.Marker({
           map: map,
+          url: place.icon,
           icon: icon,
           title: place.name,
           position: place.geometry.location
@@ -76,57 +74,32 @@ function initAutocomplete() {
           bounds.extend(place.geometry.location);
         }
       });
+
       map.fitBounds(bounds);
-    });
-  }
+      });
+
+//    let request = {
+//        placeId: input,
+//        fields: ['name', 'formatted_address', 'place_id', 'geometry']
+//      }
+
+//   let infowindow = new google.maps.InfoWindow(); 
+//   let service = new google.maps.places.PlacesService(map);
+
+//   service.getDetails(request, function(place, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//         let marker = new google.maps.Marker({
+//           map: map,
+//           position: place.geometry.location
+//           });
+//       }
+//     google.maps.event.addListener(marker, 'click', function() {
+//         infowindow.setContent('<div><strong>' + place.name + '</strong><br' + 
+//           'Place ID: ' + place.place_id + '<br>' + 
+//           place.formatted_addresss + '</div>');
+//           infowindow.open(map, this);
+//           });
+//       }); 
+}
 
 $(navigationListener);
-
-// OLD CODE BELOW, FETCH DID NOT WORK BECAUSE OF HOW THE API WORKS. NEEDED TO USE SPECIFIC MAPS JS API LIBRARY.
-
-// function submitFoodBank() {
-//     console.log("app loaded!");
-//     $("#food-bank-selector").on("submit", function(event) {
-//         event.preventDefault();
-//         const bankEntry = $(this).find("#food-bank").val();
-//         getFoodBank(bankEntry);
-//     });
-// }
-
-// function convertQuery(parameters) {
-//     const searchData = Object.keys(parameters).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(parameters[key])}`);
-//     return searchData.join("&");
-// }
-
-// function getFoodBank(bankEntry) {
-//     const parameters = { 
-//         input: bankEntry,
-//         inputtype: 'textquery',
-//         fields: ['formatted_address', 'geometry', 'icon,name', 'permanently_closed', 'photos', 'place_id', 'plus_code', 'types'],
-//         key: testKey,
-//     };
-
-//     const searchString = convertQuery(parameters);
-//     const searchQuery = foodBankSearch + '?' + searchString;
-//     console.log(searchQuery);
-
-//     fetch(searchQuery)
-//         .then(response => {
-//             if (response.ok) {
-//                 return response.json();
-//             }
-//             throw new Error(response.statusText);
-//             })
-//         .then(responseJson => showFoodBank(responseJson))
-//         .catch(error => alert("Something went wrong. Try again later."));
-// }
-
-// function showFoodBank(responseJson) {
-//     console.log(responseJson);
-//     $("#food-bank-list").empty();
-//     $("#food-bank-list").append(`
-//     <li><h3>${responseJson.data.name}</h3>`);
-//     $("#search-results").removeClass("hidden");
-// }
-
-// $(submitFoodBank);
