dba8e2a033b2085981e423b0607616fe5c64e69f
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-02-22 12:58:30 -0800

    version 2 of mvp

--- index.js
+++ index.js
@@ -33,72 +33,76 @@ function initAutocomplete() {
         gestureHandling: 'auto',
         maptypeId: 'roadmap'
     });
-
-    let input = document.getElementById('pac-input'); 
-    let searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  
-    map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
-    });
-
-    searchBox.addListener('places_changed', function() {
-      let places = searchBox.getPlaces();
-  
-      if (places.length == 0) {
-        return;
-      }
-
-    let service = new google.maps.places.PlacesService(map);
-    let infowindow = new google.maps.InfoWindow();  
-
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers= [];
-
-      let bounds = new google.maps.LatLngBounds();
-
-      places.forEach(function(place) {
-        let marker = new google.maps.Marker({
-          map: map,
-          title: place.name,
-          position: place.geometry.location,
-          placeId: place.place_id
-        });
-
-      markers.push(marker);
-
-      google.maps.event.addListener(marker, 'click', function(event) {
-          service.getDetails({placeId: this.placeId}, (function(marker) {
-            return function(place, status) {
-              if (status === google.maps.places.PlacesServiceStatus.OK) {
-                infowindow.setContent(
-                  `<div id="info"> 
-                  <span id="name"><b>${place.name}</b></span>
-                  <br>
-                  <span id="address">${place.formatted_address}</span>
-                  <br>
-                  <span id="website"><a href=${place.website}>Website</a></span>
-                  <br>
-                  <span id="google-map"><a href=${place.url}>Open in Google Maps</a></span>
-                  </div>`);
-                infowindow.open(map, marker);
-                }
-            }
-          }
-        (marker)));
-      });
-
-      if (place.geometry.viewport) {
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    
-    });
-  map.fitBounds(bounds);
-    });
-  }
+}
 
 $(navigationListener);
+
+
+// OLD CODE FROM FIRST VERSION: 
+
+// let input = document.getElementById('pac-input'); 
+// let searchBox = new google.maps.places.SearchBox(input);
+// map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+// map.addListener('bounds_changed', function() {
+//   searchBox.setBounds(map.getBounds());
+// });
+
+// searchBox.addListener('places_changed', function() {
+//   let places = searchBox.getPlaces();
+
+//   if (places.length == 0) {
+//     return;
+//   }
+
+// let service = new google.maps.places.PlacesService(map);
+// let infowindow = new google.maps.InfoWindow();  
+
+//   markers.forEach(function(marker) {
+//     marker.setMap(null);
+//   });
+//   markers= [];
+
+//   let bounds = new google.maps.LatLngBounds();
+
+//   places.forEach(function(place) {
+//     let marker = new google.maps.Marker({
+//       map: map,
+//       title: place.name,
+//       position: place.geometry.location,
+//       placeId: place.place_id
+//     });
+
+//   markers.push(marker);
+
+//   google.maps.event.addListener(marker, 'click', function(event) {
+//       service.getDetails({placeId: this.placeId}, (function(marker) {
+//         return function(place, status) {
+//           if (status === google.maps.places.PlacesServiceStatus.OK) {
+//             infowindow.setContent(
+//               `<div id="info"> 
+//               <span id="name"><b>${place.name}</b></span>
+//               <br>
+//               <span id="address">${place.formatted_address}</span>
+//               <br>
+//               <span id="website"><a href=${place.website}>Website</a></span>
+//               <br>
+//               <span id="google-map"><a href=${place.url}>Open in Google Maps</a></span>
+//               </div>`);
+//             infowindow.open(map, marker);
+//             }
+//         }
+//       }
+//     (marker)));
+//   });
+
+//   if (place.geometry.viewport) {
+//     bounds.union(place.geometry.viewport);
+//   } else {
+//     bounds.extend(place.geometry.location);
+//   }
+
+// });
+// map.fitBounds(bounds);
+// });
+// }
